d3a5bd2f96cd3356ccb69e00bf4459cdbb08f63b
Teodor Ivanov <teodor.ivanov.2498@gmail.com>
2025-07-10 17:32:39 +0300

    Email backend update

--- MerchantSite/settings.py
+++ MerchantSite/settings.py
@@ -98,6 +98,7 @@ LOGOUT_REDIRECT_URL = 'login'
 
 # EMAIL SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
